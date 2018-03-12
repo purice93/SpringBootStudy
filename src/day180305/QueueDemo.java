7394752854b14b1bdc5d47bfee4fb1bb5671bdd5
purice93 <530337704@qq.com>
2018-03-12 22:03:22 +0800

    Initial commit

---
+++ src/day180305/QueueDemo.java
@@ -0,0 +1,13 @@
+package day180305;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+/**
+ * @author: ZouTai
+ * @date: 2018/3/5
+ * @description:
+ */
+public class QueueDemo {
+    BlockingQueue bq = new ArrayBlockingQueue(10);
+}
